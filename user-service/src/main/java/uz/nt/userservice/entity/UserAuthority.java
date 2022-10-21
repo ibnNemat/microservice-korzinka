48542681c5e56b384c7a675dc9a602f91a63d119
Ibrohim <ahmadjonovibrohim063@gmail.com>
2022-10-21 08:23:38 +0500

    User-service files added

---
+++ user-service/src/main/java/uz/nt/userservice/entity/UserAuthority.java
@@ -0,0 +1,24 @@
+package uz.nt.userservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_authority_id_seq")
+    @SequenceGenerator(name = "user_authority_id_seq", sequenceName = "user_authority_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    private Integer user_id;
+    private Integer authority_id;
+}
