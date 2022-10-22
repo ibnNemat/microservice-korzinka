d281edaad2109af6f6413f9104278fae883e57ab
boburmirzaqulov <ab200mirzaqulov@gmail.com>
2022-10-22 11:44:25 +0500

    cloud-config-server

--- delivery-service/src/main/java/com/example/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/example/deliveryservice/DeliveryServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class DeliveryServiceApplication {
 
     public static void main(String[] args) {
