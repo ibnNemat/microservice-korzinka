c8b16d7bd19d2beeabc0a513950980021c8bb765
Abdumalik <abdumaliksobirov007@gmail.com>
2022-10-23 17:24:28 +0500

    LocalCard entity, repository, service ochildi

---
+++ shared-libs/src/main/java/shared/libs/dto/LocalCardDto.java
@@ -0,0 +1,17 @@
+package shared.libs.dto;
+
+import java.util.Date;
+
+public class LocalCardDto {
+
+    private Integer id;
+
+    private Date issuedDate;
+
+    private Date expireDate;
+
+    private String userPhoneNumber;
+
+    private Double total;
+
+}
