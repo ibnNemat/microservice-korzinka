67bb9a01ef4594f7a7c34662923b9d1e5dde70c4
Abdumalik <abdumaliksobirov007@gmail.com>
2022-10-23 17:24:28 +0500

    LocalCard entity, repository, service ochildi

---
+++ cashback-service/src/main/java/uz/nt/cashbackservice/controller/LocalCardController.java
@@ -0,0 +1,11 @@
+package uz.nt.cashbackservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/local-card")
+public class LocalCardController {
+
+
+}
