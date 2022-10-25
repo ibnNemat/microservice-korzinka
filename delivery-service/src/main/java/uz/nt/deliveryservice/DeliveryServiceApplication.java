a21fe6380b66b5f6ff46ebed414960d3d1de1a6b
sirojiddin <nematovsr@gmail.com>
2022-10-25 11:49:26 +0500

    Delivery Service

--- delivery-service/src/main/java/uz/nt/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/uz/nt/deliveryservice/DeliveryServiceApplication.java
@@ -3,9 +3,13 @@ package uz.nt.deliveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+//@ComponentScan({"shared.libs.security", "uz.nt.deliveryservice"})
+//@EnableRedisRepositories({"shared.libs.security", "uz.nt.deliveryservice"})
 public class DeliveryServiceApplication {
 
     public static void main(String[] args) {
