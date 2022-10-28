45ee3d4c3b317e96674e9d74211fc4019f21a886
Yusuf <yusufmahmudov909@gmail.com>
2022-10-28 09:53:11 +0500

    commit for order-service

--- order-service/src/main/java/uz/nt/orderservice/repository/OrderedProductsRedisRepository.java
+++ order-service/src/main/java/uz/nt/orderservice/repository/OrderedProductsRedisRepository.java
@@ -2,7 +2,12 @@ package uz.nt.orderservice.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import uz.nt.orderservice.dto.OrderedProductsDetail;
 import uz.nt.orderservice.entity.OrderedProductsRedis;
-@Component
-public interface OrderedProductsRedisRepository extends CrudRepository<OrderedProductsRedis, Long> {
+
+import java.util.List;
+
+@Repository
+public interface OrderedProductsRedisRepository extends CrudRepository<List<OrderedProductsDetail>, Integer> {
 }
