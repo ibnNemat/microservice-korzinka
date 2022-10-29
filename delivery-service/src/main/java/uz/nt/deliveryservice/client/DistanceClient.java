dfd70dd01ea3ab3655436308033357c1d3e54bb3
Yusupov-Toshpolat <toshpolaty@gmail.com>
2022-10-29 10:54:29 +0300

    check API two points distance

--- delivery-service/src/main/java/uz/nt/deliveryservice/client/DistanceClient.java
+++ delivery-service/src/main/java/uz/nt/deliveryservice/client/DistanceClient.java
@@ -1,16 +1,12 @@
 package uz.nt.deliveryservice.client;
 
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import shared.libs.dto.DistanceResponse;
 import shared.libs.dto.distance.DistanceInfo;
 
 @FeignClient(value = "distance-service", url = "https://api.distancematrix.ai/maps/api")
-public interface RapidClient {
+public interface DistanceClient {
 
     @GetMapping("/distancematrix/json")
     DistanceInfo getResponseBetweenTwoPoints(@RequestParam(value = "origins") String origins,
