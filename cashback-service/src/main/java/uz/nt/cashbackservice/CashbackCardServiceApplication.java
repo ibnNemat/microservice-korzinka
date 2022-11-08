50a9d37547658ead050cd2f91c1d21941a8c689e
shohrux <@king03official>
2022-11-08 09:35:28 +0500

    v

--- cashback-service/src/main/java/uz/nt/cashbackservice/CashbackCardServiceApplication.java
+++ cashback-service/src/main/java/uz/nt/cashbackservice/CashbackCardServiceApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
-public class CashbackCardServiceApplication {
+@EnableConfigurationProperties
+@ComponentScan({"shared.libs", "uz.nt.deliveryservice"})
+@EnableRedisRepositories({"shared.libs", "uz.nt.deliveryservice"})public class CashbackCardServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CashbackCardServiceApplication.class, args);
