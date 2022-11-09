e354ce8b64ae41cc12fe3b2e3f17bbd833829bcb
Abdumalik <abdumaliksobirov007@gmail.com>
2022-11-09 13:25:06 +0500

    Yangilandi

--- cashback-service/src/main/java/uz/nt/cashbackservice/repository/CashbackCardRepository.java
+++ cashback-service/src/main/java/uz/nt/cashbackservice/repository/CashbackCardRepository.java
@@ -1,15 +1,14 @@
 package uz.nt.cashbackservice.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uz.nt.cashbackservice.entity.CashbackCard;
 
 @Repository
 public interface CashbackCardRepository extends JpaRepository<CashbackCard, Integer> {
-
 
     @Transactional
     void deleteCashbackCardByUserId(Integer userId);
